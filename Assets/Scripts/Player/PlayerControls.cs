91b6aec6521eb8ed93e8b9627529e96f7bbb59a2
Martin Georgiev <martin.tonin@gmail.com>
2022-02-02 00:09:24 +0000

    Collision detection
    Added collision detection and laser firing . Now lasers only fire when fire buttons are pressed and already existing particles remain in world until they expire.

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -6,17 +6,31 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    [SerializeField] InputAction movement;
-    [SerializeField] InputAction fire;
+    [Header("General Setup")]
 
+    [Tooltip("How fast ship along screen")]
     [SerializeField] float controlSpeed = 30f;
+    [Tooltip("Range of movement along vertical axis")]
     [SerializeField] float xRange = 13f;
+    [Tooltip("Range of movement along Horizonal axis")]
     [SerializeField] float yRange = 8f;
+    [Header("Laser gun array")]
+    [Tooltip("Add all player lasers here")]
+    [SerializeField] GameObject[] lasers;
 
+    [Header("Screen position based tuning")]
     [SerializeField] float positionPitchFactor = -3f;
-    [SerializeField] float controlPitchFactor = -15f;
     [SerializeField] float positionYawFactor = 2.3f;
+
+    [Header("Player input based tuning")]
+    [SerializeField] float controlPitchFactor = -15f;
     [SerializeField] float controlRollFactor = -20f;
+
+    [Header("Ship input configurations")]
+    [Tooltip("Input options for movement")]
+    [SerializeField] InputAction movement;
+    [Tooltip("Input options for firing weapons")]
+    [SerializeField] InputAction fire;
 
     float xThrow, yThrow;
 
@@ -74,14 +88,20 @@ public class PlayerControls : MonoBehaviour
     {
         if (fire.ReadValue<float>() > 0.5)
         {
-            Debug.Log("Button is Pressed");
+            SetLasersActive(true);
         }
         else
         {
-            Debug.Log("Button not pressed");
+            SetLasersActive(false);
         }
-        //if pushing fire button
-        //Then print shoooting
-        //or don't print shooting
+    }
+
+    private void SetLasersActive(Boolean state)
+    {
+        foreach (GameObject laser in lasers)
+        {
+            var emission = laser.GetComponent<ParticleSystem>().emission;
+            emission.enabled = state;
+        }
     }
 }
